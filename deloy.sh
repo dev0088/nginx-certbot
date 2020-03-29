0e2a7123c9b418b3746e727f06457972f0f311fc
dev0088 <ninjadev999@gmail.com>
2020-03-28 21:33:20 -0700

    Updated domain.

---
+++ deloy.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#############################
+# Deploy with docker-compose mode
+#############################
+
+# Stop previous dockers
+docker-compose stop
+docker-compose down
+
+# Re-generate certificates with domain
+./init-letsencrypt.sh
+
+# Run dockers as daemon
+docker-compose up -d --build
+
+# Remove caches
+yes | docker system prune
+
